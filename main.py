ab0abfe4c91a6bce7f28ac6ecb964a335e18095b
darksidekraken1337 <daniilnstatsenko@gmail.com>
2024-01-28 14:22:16 +0300

    окно

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print('z')
-print('esh')
+import pygame
+import sys
+
+def run():
+
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('SOSU GAME')
+    bg_color = (0, 0, 0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()
+run()
