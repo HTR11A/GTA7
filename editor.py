18653d6226545418374034c74d9fbef2ee211302
HTR11A <113211778+HTR11A@users.noreply.github.com>
2024-03-17 23:10:42 +0300

    обновление

--- editor.py
+++ editor.py
@@ -165,10 +165,9 @@ def del_circle(args, cur_circles):
     for i in range(len(cur_circles)):
         centre = cur_circles[i]['X_pos']
         a, b = centre[0], centre[1]  # координаты центра круга
-        if cur_circles[i]['Radius'] > 0:
-            r = cur_circles[i]['Radius']
-            if abs(a - x) ** 2 + abs(b - y) ** 2 <= r ** 2:
-                return cur_circles[i]['ID']
+        r = cur_circles[i]['Radius']
+        if r > 0 and abs(a - x) ** 2 + abs(b - y) ** 2 <= r ** 2:
+            return cur_circles[i]['ID']
 
 
 def save_level(circles, audio_path, image_path, directory):
